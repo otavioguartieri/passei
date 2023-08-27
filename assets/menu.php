e975f627070db4ed46a420e3a473b8276d765cc8
otavioguartieri <otaviorg10@gmail.com>
2023-08-27 13:00:29 -0300

    menu items on closed menu

--- assets/menu.php
+++ assets/menu.php
@@ -1,1 +1,16 @@
-menu
+<div class="menu-item" onclick="alert('a');">
+    <div class="menu-item-icon"><i class="fa-solid fa-house"></i></div>
+    <div class="menu-item-name">home</div>
+</div>
+<div class="menu-item" onclick="alert('a');">
+    <div class="menu-item-icon"><i class="fa-solid fa-house"></i></div>
+    <div class="menu-item-name">home casa quebrado paraleepipedo</div>
+</div>
+<div class="menu-item" onclick="alert('a');">
+    <div class="menu-item-icon"><i class="fa-solid fa-house"></i></div>
+    <div class="menu-item-name">home</div>
+</div>
+<div class="menu-item" onclick="alert('a');">
+    <div class="menu-item-icon"><i class="fa-solid fa-house"></i></div>
+    <div class="menu-item-name">home</div>
+</div>
